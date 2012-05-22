330846b63d653dfa15568d1d3144a6d71213503e
Ben Foxall <benfoxall@gmail.com>
2012-05-22 09:43:01 +0100

    working...

--- app.js
+++ app.js
@@ -2,16 +2,57 @@
 var serialport = require("serialport");
 var SerialPort = serialport.SerialPort;
 
-var sp = new SerialPort("/dev/tty.usbmodem621", { 
+var sp = new SerialPort("/dev/tty.usbmodem641", { 
     parser: serialport.parsers.readline("\n") 
   });
 
 sp.on("data", function (data) {
-	console.log("here: "+data);
+	console.log(">> "+data);
 });
 
-setTimeout(function(){
-	sp.write("111!!");
-	sp.write("431!!!");
-	sp.write("23445!!!!");
-}, 1500);
+write = function(n){
+	console.log('<<' + n + '!!')
+	sp.write(n + '!!');
+};
+
+
+
+var express = require('express'),
+    app = express.createServer();
+    app.use(express.bodyParser());
+ 
+app.configure(function () {
+	app.use(express.static(__dirname + '/public'));
+	app.use(express.bodyParser());
+});
+
+var reset = 28113973;
+var mapWhat = {
+	'broke the build':17,
+	'needs coffee':19,
+	'needs beer':21
+};
+var mapWho = [15,13,11,9,7,5,3,1];
+
+app.post('/', function(req, res){
+	write(reset);
+	write(mapWhat[req.body.what]);
+	write(mapWho[req.body.who]);
+	res.redirect('/');
+});
+
+app.post('/reset', function(req,res){
+	write(reset);
+	res.redirect('/')
+})
+
+
+
+var people = "WhiteOctober benjaminbenben johnwards mightjustwork peterjwest ptrmcc rich_81 skinofstars".split(' ');
+
+app.get('/people', function(req, res){
+	res.send(people);
+});
+
+
+app.listen(3000);
